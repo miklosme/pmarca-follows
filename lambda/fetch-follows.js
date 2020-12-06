2e345498735c74205b7830909be9e7fa64bf3782
Miklos Megyes <m.miklos05@gmail.com>
2020-12-06 15:57:10 +0100

    fix fetch-follows

--- lambda/fetch-follows.js
+++ lambda/fetch-follows.js
@@ -1,5 +1,6 @@
-const fetch = require('node-fetch');
-const qs = require('qs');
+const { DynamoDB } = require('aws-sdk');
+const https = require('https');
+const qs = require('querystring');
 
 const URL = 'https://api.twitter.com/1.1/friends/list.json';
 const PMARCA_ID = 5943622;
@@ -8,16 +9,61 @@ exports.handler = async event => {
     const params = {
         user_id: PMARCA_ID,
     };
-    const res = await fetch(`${URL}?${qs.stringify(params)}`, {
-        method: 'GET',
-        headers: {
-            authorization: `Bearer ${process.env.TWITTER_TOKEN}`,
-        },
+
+    const data = await new Promise((resolve, reject) => {
+        let dataString = '';
+        const req = https.get(
+            `${URL}?${qs.stringify(params)}`,
+            {
+                headers: {
+                    authorization: `Bearer ${process.env.TWITTER_TOKEN}`,
+                },
+            },
+            res => {
+                res.on('data', chunk => {
+                    dataString += chunk;
+                });
+                res.on('end', () => {
+                    resolve(JSON.parse(dataString));
+                });
+            },
+        );
+
+        req.on('error', e => {
+            reject(e);
+        });
     });
 
-    const data = await res.json();
+    console.log(
+        data.users.map(u => ({
+            id: u.id,
+            name: u.name,
+        })),
+    );
 
-    console.log(data.users.map(u => u.name));
+    const dynamo = new DynamoDB();
+
+    await dynamo
+        .batchWriteItem({
+            RequestItems: {
+                [process.env.FOLLOWS_TABLE_NAME]: data.users.map(user => ({
+                    PutRequest: {
+                        Item: {
+                            id: { N: String(user.id) },
+                            name: { S: user.name },
+                        },
+                    },
+                })),
+            },
+        })
+        .promise();
+
+    // await dynamo.updateItem({
+    //   TableName: process.env.HITS_TABLE_NAME,
+    //   Key: { path: { S: event.path } },
+    //   UpdateExpression: 'ADD hits :incr',
+    //   ExpressionAttributeValues: { ':incr': { N: '1' } }
+    // }).promise();
 
     return {
         statusCode: 200,
--- lib/pmarca-follows-stack.ts
+++ lib/pmarca-follows-stack.ts
@@ -1,9 +1,38 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as apigw from '@aws-cdk/aws-apigateway';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
+
+require('dotenv').config();
 
 export class PmarcaFollowsStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+        const followsTable = new dynamodb.Table(this, 'Follows', {
+            partitionKey: { name: 'id', type: dynamodb.AttributeType.NUMBER },
+        });
+
+        const fetchFollows = new lambda.Function(this, 'FetchFollows', {
+            runtime: lambda.Runtime.NODEJS_12_X,
+            code: lambda.Code.fromAsset('lambda'),
+            handler: 'fetch-follows.handler',
+            environment: {
+                TWITTER_TOKEN: process.env.TWITTER_TOKEN as string,
+                FOLLOWS_TABLE_NAME: followsTable.tableName,
+            },
+        });
+
+        followsTable.grantReadWriteData(fetchFollows);
+
+        new apigw.LambdaRestApi(this, 'Endpoint', {
+            handler: fetchFollows,
+        });
+
+        new TableViewer(this, 'ViewFollowers', {
+            title: 'Pmarca Follows',
+            table: followsTable,
+        });
+    }
 }
