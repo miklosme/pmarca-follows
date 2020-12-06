3415a33c0010f50c1900dd31eaf68c7d27c989cb
Miklos Megyes <m.miklos05@gmail.com>
2020-12-06 19:55:42 +0100

    new follows are processed only

--- lambda/fetch-follows.js
+++ lambda/fetch-follows.js
@@ -4,6 +4,7 @@ const qs = require('querystring');
 
 const URL = 'https://api.twitter.com/1.1/friends/list.json';
 const PMARCA_ID = 5943622;
+const TABLE = process.env.FOLLOWS_TABLE_NAME;
 
 exports.handler = async event => {
     const params = {
@@ -33,41 +34,59 @@ exports.handler = async event => {
             reject(e);
         });
     });
-
-    console.log(
-        data.users.map(u => ({
-            id: u.id,
-            name: u.name,
-        })),
-    );
-
     const dynamo = new DynamoDB();
 
-    await dynamo
-        .batchWriteItem({
+    const lastFollows = data.users.map(u => String(u.id));
+    const lastFollowsQuery = await dynamo
+        .batchGetItem({
             RequestItems: {
-                [process.env.FOLLOWS_TABLE_NAME]: data.users.map(user => ({
-                    PutRequest: {
-                        Item: {
-                            id: { N: String(user.id) },
-                            name: { S: user.name },
-                        },
-                    },
-                })),
+                [TABLE]: {
+                    Keys: lastFollows.map(id => ({
+                        id: { N: id },
+                    })),
+                },
             },
         })
         .promise();
 
-    // await dynamo.updateItem({
-    //   TableName: process.env.HITS_TABLE_NAME,
-    //   Key: { path: { S: event.path } },
-    //   UpdateExpression: 'ADD hits :incr',
-    //   ExpressionAttributeValues: { ':incr': { N: '1' } }
-    // }).promise();
+    const knownIdsSet = new Set(lastFollowsQuery['Responses'][TABLE].map(x => String(x['id']['N'])));
+    const newFollowsIdsSet = new Set(lastFollows.filter(id => !knownIdsSet.has(id)));
+
+    const datetime = new Date().getTime().toString();
+
+    const newUsers = data.users
+        .filter(user => newFollowsIdsSet.has(String(user.id)))
+        .map(user => ({ id: user.id, name: user.name }));
+
+    if (newUsers.length) {
+        await dynamo
+            .batchWriteItem({
+                RequestItems: {
+                    [TABLE]: newUsers.map(user => ({
+                        PutRequest: {
+                            Item: {
+                                id: { N: String(user.id) },
+                                name: { S: user.name },
+                                date: { S: datetime },
+                            },
+                        },
+                    })),
+                },
+            })
+            .promise();
+    }
+
+    const result = JSON.stringify(
+        {
+            newUsers,
+        },
+        null,
+        2,
+    );
 
     return {
         statusCode: 200,
         headers: { 'Content-Type': 'text/plain' },
-        body: `Hello, CDK! You've hit ${event.path}\n`,
+        body: `Follow fetch successful: ${result}\n`,
     };
 };
